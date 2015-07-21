4a62b85bb3388b05349c4363a9ca03a819041309
Craig Boyce <craig.boyce@gmail.com>
2015-07-21 11:36:16 -0400

    Initial Commit

---
+++ GoBoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoBoop
+//
+//  Created by Craig on 7/21/15.
+//  Copyright (c) 2015 Internet and Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
