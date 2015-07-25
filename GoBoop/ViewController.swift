7e59f8ff65da4f04f5d198240266af64e9d95de0
Craig Boyce <craig.boyce@gmail.com>
2015-07-25 10:16:40 -0400

    Saving to be able to back out of auto layout changes

--- GoBoop/ViewController.swift
+++ GoBoop/ViewController.swift
@@ -9,19 +9,29 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
     @IBOutlet weak var noseAnimation: UIImageView!
+    @IBOutlet weak var pointingFinger: UIImageView!
     let ukePlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, error: nil)
-        
-        // Do any additional setup after loading the view, typically from a nib.
+ 
     }
     
+//    override func viewWillAppear(animated: Bool) {
+//    }
+    
     override func viewDidAppear(animated: Bool) {
+        let screenCenterV = CGFloat(UIScreen.mainScreen().bounds.height / 2)
+        let screenCenterH = UIScreen.mainScreen().bounds.width / 2
+        let fingerCenterV = screenCenterV - (pointingFinger.bounds.height / 2)
+        var fingerPoint: CGPoint = CGPointMake(UIScreen.mainScreen().bounds.width - 10.0, fingerCenterV)
+        pointingFinger.frame = CGRectMake(100.0, 100.0, pointingFinger.bounds.width, pointingFinger.bounds.height)
+
+        
         var imageArray: [UIImage] = []
         let image1 = UIImage(named: "NoseImage1")
         let image2 = UIImage(named: "NoseImage2")
